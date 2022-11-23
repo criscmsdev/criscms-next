24a2af06f8fc75d6b176254ff0fb1d651a545525
Jesus Calamani <criscmsdev@gmail.com>
2022-11-23 00:26:17 -0400

    v

--- graphql/graphqlClient.ts
+++ graphql/graphqlClient.ts
@@ -1,8 +1,8 @@
 import { GraphQLClient } from "graphql-request";
 
 export const graphQLClient = new GraphQLClient(
-  `http://localhost:6002/graphql`,
-  // `${process.env.API_URL}/graphql`,
+  // `http://localhost:6002/graphql`,
+  `${process.env.API_URL}/graphql`,
   {
     headers: {
       authorization: "Bearer MY_TOKEN",
